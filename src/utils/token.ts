4baa023d9149a8aaa9fefd4a566fd60c6e385339
Lizhou <childlike1world@gmail.com>
2024-02-27 12:33:19 -0800

    minor change to include both local and cloud version

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,3 +1,4 @@
+// both local and aws works this way
 export const setToken = (token: string) => {};
 
 export const getToken = () => {
@@ -5,3 +6,15 @@ export const getToken = () => {
 };
 
 export const clearToken = () => localStorage.clear();
+
+// local version
+// const TOKEN = "token";
+
+// export const setToken = (token: string) => localStorage.setItem(TOKEN, token);
+
+// export const getToken = () => {
+//   const token = localStorage.getItem(TOKEN);
+//   return token ? `Bearer ${token}` : "";
+// };
+
+// export const clearToken = () => localStorage.clear();
